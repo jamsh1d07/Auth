3e3f1b2475a165e5fd29cc435b398b2162e89053
Jamshid <raimovjamshid0704@gmail.com>
2023-10-13 17:12:47 +0500

    Commit finalllll

--- src/main/java/uz/pdp/security/config/OpenApiConfig.java
+++ src/main/java/uz/pdp/security/config/OpenApiConfig.java
@@ -29,7 +29,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
         servers = {
                 @Server(
                         description = "Local ENV",
-                        url = "http://localhost:8080"
+                        url = "https://auth-production-4e00.up.railway.app"
                 )
         },
         security = {
