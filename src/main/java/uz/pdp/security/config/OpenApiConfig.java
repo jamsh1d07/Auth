5106c27c675a7e514edf657e4b66356d82d21cf5
Jamshid <raimovjamshid0704@gmail.com>
2023-10-13 16:52:57 +0500

    Commit finallll

--- src/main/java/uz/pdp/security/config/OpenApiConfig.java
+++ src/main/java/uz/pdp/security/config/OpenApiConfig.java
@@ -30,10 +30,6 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 @Server(
                         description = "Local ENV",
                         url = "http://localhost:8080"
-                ),
-                @Server(
-                        description = "PROD ENV",
-                        url = "https://aliboucoding.com/course"
                 )
         },
         security = {
