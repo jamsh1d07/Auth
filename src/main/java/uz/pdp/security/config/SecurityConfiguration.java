049cf8681deabef9819b8bc156589e36c3217811
Jamshid <raimovjamshid0704@gmail.com>
2024-01-06 15:25:07 +0500

    Commit finallllllllll

--- src/main/java/uz/pdp/security/config/SecurityConfiguration.java
+++ src/main/java/uz/pdp/security/config/SecurityConfiguration.java
@@ -13,8 +13,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 import static uz.pdp.security.user.Permission.*;
 import static uz.pdp.security.user.Role.ADMIN;
@@ -63,7 +66,13 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
+                .cors(cors-> cors.configurationSource(request -> {
+                    CorsConfiguration configuration=new CorsConfiguration();
+                    configuration.setAllowedOrigins(Arrays.asList("*"));
+                    configuration.setAllowedMethods(Arrays.asList("*"));
+                    configuration.setAllowedHeaders(Arrays.asList("*"));
+                    return configuration;
+                }))
                 .authorizeHttpRequests(req ->
                         req.requestMatchers(WHITE_LIST_URL)
                                 .permitAll()
